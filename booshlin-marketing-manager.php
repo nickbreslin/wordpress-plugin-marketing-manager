39800c6b23aa55582c9c6c256e022fab266b9112
Nick Breslin <nickbreslin@gmail.com>
2017-09-01 15:40:56 -0700

    Updating for marketing manager naming

--- booshlin-marketing-manager.php
+++ booshlin-marketing-manager.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * The plugin bootstrap file
  *
@@ -10,18 +9,18 @@
  *
  * @link              http://example.com
  * @since             1.0.0
- * @package           Plugin_Name
+ * @package           Booshlin Marketing Manager
  *
  * @wordpress-plugin
- * Plugin Name:       WordPress Plugin Boilerplate
- * Plugin URI:        http://example.com/plugin-name-uri/
+ * Plugin Name:       Booshlin Marketing Manager
+ * Plugin URI:        http://www.booshlin.com/marketing-manager/
  * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
  * Version:           1.0.0
- * Author:            Your Name or Your Company
- * Author URI:        http://example.com/
+ * Author:            Nick Breslin
+ * Author URI:        https://github.com/nickbreslin
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
- * Text Domain:       plugin-name
+ * Text Domain:       booshlin-marketing-manager
  * Domain Path:       /languages
  */
 
@@ -34,30 +33,30 @@ define( PLUGIN_VERSION, '1.0.0' );
 
 /**
  * The code that runs during plugin activation.
- * This action is documented in includes/class-plugin-name-activator.php
+ * This action is documented in includes/class-booshlin-marketing-manager-activator.php
  */
-function activate_plugin_name() {
-	require_once plugin_dir_path( __FILE__ ) . 'includes/class-plugin-name-activator.php';
-	Plugin_Name_Activator::activate();
+function activate_booshlin_marketing_manager() {
+	require_once plugin_dir_path( __FILE__ ) . 'includes/class-booshlin-marketing-manager-activator.php';
+	Booshlin_Marketing_Manager_Activator::activate();
 }
 
 /**
  * The code that runs during plugin deactivation.
- * This action is documented in includes/class-plugin-name-deactivator.php
+ * This action is documented in includes/class-booshlin_marketing_manager-deactivator.php
  */
-function deactivate_plugin_name() {
-	require_once plugin_dir_path( __FILE__ ) . 'includes/class-plugin-name-deactivator.php';
-	Plugin_Name_Deactivator::deactivate();
+function deactivate_booshlin_marketing_manager() {
+	require_once plugin_dir_path( __FILE__ ) . 'includes/class-booshlin-marketing-manager-deactivator.php';
+	Booshlin_Marketing_Manager_Deactivator::deactivate();
 }
 
-register_activation_hook( __FILE__, 'activate_plugin_name' );
-register_deactivation_hook( __FILE__, 'deactivate_plugin_name' );
+register_activation_hook( __FILE__, 'activate_booshlin_marketing_manager' );
+register_deactivation_hook( __FILE__, 'deactivate_booshlin_marketing_manager' );
 
 /**
  * The core plugin class that is used to define internationalization,
  * admin-specific hooks, and public-facing site hooks.
  */
-require plugin_dir_path( __FILE__ ) . 'includes/class-plugin-name.php';
+require plugin_dir_path( __FILE__ ) . 'includes/class-booshlin_marketing_manager.php';
 
 /**
  * Begins execution of the plugin.
@@ -68,10 +67,11 @@ require plugin_dir_path( __FILE__ ) . 'includes/class-plugin-name.php';
  *
  * @since    1.0.0
  */
-function run_plugin_name() {
+function run_booshlin_marketing_manager() {
 
-	$plugin = new Plugin_Name();
+	$plugin = new Booshlin_Margeting_Manager();
 	$plugin->run();
 
 }
-run_plugin_name();
+
+run_booshlin_marketing_manager();
